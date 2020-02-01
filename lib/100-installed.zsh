993737f9bb196d27c80e52a748615c59fdc0afa6
Joseph Linder <joemlinder@gmail.com>
2020-02-01 09:56:29 -0600

    updated linux dotfiles and refactored install scripts

--- lib/100-installed.zsh
+++ lib/100-installed.zsh
@@ -13,33 +13,16 @@ export GIT_PAGER='less -m -X --quit-at-eof'
 # Mac
 if [ $(uname -s) = "Darwin" ]; then
     source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh
-
-# Linux
-else
-    source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh
-fi
-
-# https://github.com/zsh-users/zsh-syntax-highlighting/issues/411#issuecomment-317109904
-zle -N history-substring-search-up; zle -N history-substring-search-down
-
-# zsh syntax highlighting
-# MacOS
-if [ $(uname -s) = "Darwin" ]; then
     source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-
-# Linux
-else
-    source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-fi
-
-# must come after zsh-syntax-highlighting
-# zsh history substring search
-# MacOS
-if [ $(uname -s) = "Darwin"  ]; then
     source /usr/local/share/zsh-history-substring-search/zsh-history-substring-search.zsh
 
 # Linux
 else
+    source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+    source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
     source /usr/share/zsh-history-substring-search/zsh-history-substring-search.zsh
 fi
+
+# https://github.com/zsh-users/zsh-syntax-highlighting/issues/411#issuecomment-317109904
+zle -N history-substring-search-up; zle -N history-substring-search-down
 
--- tshoot/linux-bootstraps.sh
+++ tshoot/linux-bootstraps.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/bash
 
+# This is a bootstrap script I made to streamline the installation of common programs I use on Linux
+
 # Rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+# curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 # Docker
 add-apt-repository \
@@ -23,11 +25,21 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 # Update and install requirements
 sudo apt update
 sudo apt upgrade
-sudo apt install qemu-kvm libvirt-bin bridge-utils virt-managerzsh vim zsh-autosuggestions nordvpn neofetch zsh-syntax-highlighting git steam hfsprogs apt-transport-https ca-certificates curl gnupg-agent software-properties-common docker-ce docker-ce-cli containerd.io
+sudo apt install qemu-kvm libvirt-bin bridge-utils libvirt-clients libguestfs-tools libvirt-daemon-system virt-manager libosinfo-bin virtinst genisoimage zsh vim zsh-autosuggestions nordvpn neofetch zsh-syntax-highlighting steam hfsprogs apt-transport-https ca-certificates curl gnupg-agent software-properties-common docker-ce docker-ce-cli containerd.io
 
+# Setup virt-manager
 sudo adduser $(USER) libvirtd
-# Git repos
+sudo adduser $(USER) libvirt
+sudo adduser $(USER) libvirt-qemu
 
+# SSH setup
+if [ "$SSH_PORT" = "" ]; then
+    echo "No ssh port selected; using default port 22"
+else
+    sed -i '13s/^/Port $SSH_PORT\n#' /etc/ssh/sshd_config
+    echo "ssh port setup on port $SSH_PORT\n"
+
+# Git repos
 # zsh history substring
 cd ~ 
 git clone https://github.com/zsh-users/zsh-history-substring-search.git
@@ -35,8 +47,14 @@ mkdir /usr/share/zsh-history-substring-search
 mv ./zsh-history-substring-search/zsh-history-substring-search.zsh /usr/share/zsh-history-substring-search
 rm -rf ./zsh-history-substring-search
 
-# dotfiles
+# other prog-files
+cd ~
+git clone git@github.com:jmlinder/prog-files.git
+
+# dotfiles setup
 cd ~
 rm -rf .bashrc .vimrc
-git clone git@github.com:jmlinder/dotfiles.git
 ./dotfiles/bin/setup-dotfiles
+
+# End
+echo "All done...\n"
